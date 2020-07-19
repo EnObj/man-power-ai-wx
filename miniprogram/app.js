d449b0a5be5b3b023aa5b0630ae50b41326800fe
enobj <laoji52125@163.com>
2020-07-19 17:38:53 +0800

    环境id

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,8 +10,8 @@ App({
         //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
-        env: 'mc-dev-4x5dg',
-        // env: 'ma-release-koccz',
+        env: 'dev-tbr5y',
+        // env: 'prod-qu44i',
         traceUser: true,
       })
     }
