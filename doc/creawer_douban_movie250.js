eef7bf5bae9ca8b5580957cb804178b6a877fd68
enobj <laoji52125@163.com>
2020-07-20 15:38:56 +0800

    电影top250

--- doc/creawer_douban_movie250.js
+++ doc/creawer_douban_movie250.js
@@ -20,33 +20,41 @@ function loadMovies(start = 0, list = []) {
           image: $(item).find('img').attr('src'),
           link: $(item).find('a').first().attr('href'),
           content: $(item).find('span.title').first().text().trim(),
-          group: 'movie-250',
+          remarks: $(item).find('div.bd > p:nth-child(1)').text().trim().split('\n').map(item => {
+            return item.trim()
+          }).concat([
+            '评语：“' + ($(item).find('p.quote').text().trim() || '...') + '”'
+          ]),
+          group: 'movie-250-v5',
           radioOptions: ['看过', '想看']
         })
       })
       console.log(list.length)
       // 递归
-      if(list.length < 250){
+      if (list.length < 250) {
         loadMovies(start + 25, list)
-      }else{
+      } else {
         // 采集完成
         console.log('采集完成')
+        console.log(list[0])
         writeToJsonFile(list)
       }
     })
   })
 }
 
-function writeToJsonFile(list){
+function writeToJsonFile(list) {
   // 打开目标文件
-  fs.open(`/Users/jienhui/WeChatProjects/man-power-ai-wx/doc/init/mpa_content/movie-250.json`, 'w', function(err, fd){
+  fs.open(`/Users/jienhui/WeChatProjects/man-power-ai-wx/doc/init/mpa_content/movie-250-v5.json`, 'w', function (err, fd) {
     // 一行行写入
     var item = null
-    while(item = list.shift(list)){
+    while (item = list.shift(list)) {
       console.log(`+++whiting ${list.length} : ${item.content}`)
-      fs.writeFileSync(fd, JSON.stringify(item) + '\n', {flag:'a'})
+      fs.writeFileSync(fd, JSON.stringify(item) + '\n', {
+        flag: 'a'
+      })
     }
-    fs.close(fd,function(){
+    fs.close(fd, function () {
       console.log('写入文件完成')
     })
   })
