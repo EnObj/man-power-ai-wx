74afca95360ecf91c8aeb9c18633219dceb0f7b4
enobj <laoji52125@163.com>
2020-07-26 17:01:52 +0800

    @百家姓

--- doc/wpa_content_utils.js
+++ doc/wpa_content_utils.js
@@ -1,4 +1,6 @@
 const fs = require("fs")
+const http = require('http')
+const https = require('https')
 
 module.exports = {
   writeToJsonFile(list, fileName) {
@@ -17,6 +19,30 @@ module.exports = {
       }
       fs.close(fd, function () {
         console.log('写入文件完成')
+      })
+    })
+  },
+  request(url, encoding, options = {}, pipe) {
+    console.log(url)
+    const proc = url.startsWith('https') ? https : http
+    return new Promise((resolve, reject) => {
+      proc.get(url, options, function (res) {
+        if (encoding) {
+          res.setEncoding(encoding)
+        }
+        var str = "";
+        res.on("data", function (chunk) {
+          str += chunk; //监听数据响应，拼接数据片段
+        })
+        res.on("end", function () {
+          if (pipe) {
+            pipe(str).then(result => {
+              resolve(result)
+            })
+          } else {
+            resolve(str)
+          }
+        })
       })
     })
   }
