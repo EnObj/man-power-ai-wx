148b9e7f8db42fe63e6af28dd0a95bd79cf2946b
enobj <laoji52125@163.com>
2020-07-28 16:03:11 +0800

    打开单词本并搜索

---
+++ miniprogram/pages/search/search.js
@@ -0,0 +1,108 @@
+const db = wx.cloud.database()
+const pageSize = 20
+
+// miniprogram/pages/search/search.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    mpaContents: [],
+    count: 0,
+    loaded: false,
+    keyword: '',
+    more: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const keyword = decodeURIComponent(options.keyword || '大学')
+    wx.setNavigationBarTitle({
+      title: '搜索“' + keyword + "”",
+    })
+    this.setData({
+      keyword: keyword
+    })
+    this.query = db.collection('mpa_content').where({
+      group: options.group || 'daxue',
+      content: db.RegExp({
+        regexp: keyword,
+        options: 'i',
+      })
+    })
+    // 查询数目
+    this.query.count().then(res => {
+      this.setData({
+        count: res.total
+      })
+    })
+    this.loadMpaContents()
+  },
+
+  loadMpaContents(mpaContents=[]){
+    wx.showNavigationBarLoading()
+    // 查询前20条
+    this.query.orderBy('sort', 'asc').skip(mpaContents.length).limit(pageSize).get().then(res => {
+      this.setData({
+        mpaContents: mpaContents.concat(res.data),
+        loaded: true,
+        more: res.data.length == pageSize,
+      })
+      wx.hideNavigationBarLoading()
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    if(this.data.more){
+      this.loadMpaContents(this.data.mpaContents)
+    }
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ miniprogram/pages/setting/group.js
@@ -0,0 +1,174 @@
+const db = wx.cloud.database()
+
+// miniprogram/pages/setting/group.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    group: null,
+    keyword: '',
+    focus: false,
+    keywords: []
+  },
+
+  startFocusInp(event){
+    this.setData({
+      focus: true
+    })
+  },
+
+  cleanKeyword() {
+    this.setData({
+      keyword: ''
+    })
+  },
+
+  keywordChange(event) {
+    const value = event.detail.value
+    this.setData({
+      keyword: value
+    })
+  },
+
+  confirm(event) {
+    this.go(event.detail.value)
+  },
+
+  go(keyword) {
+    keyword = keyword.trim()
+    if (!keyword) {
+      wx.showToast({
+        title: '关键词不能为空',
+        icon: 'none'
+      })
+      return
+    }
+    this.setData({
+      keyword: keyword
+    })
+    setTimeout(()=>{
+      this.addToLocalHistory(keyword)
+    }, 1000)
+    wx.navigateTo({
+      url: '/pages/search/search?keyword=' + encodeURIComponent(keyword) + '&group=' + this.data.group._id
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.showNavigationBarLoading()
+    db.collection('mpa_content_group').doc(options.groupId||'daxue').get().then(res=>{
+      this.setData({
+        group: res.data
+      })
+      wx.hideNavigationBarLoading()
+    })
+    // 最近搜索
+    this.loadKeywords()
+  },
+
+  loadKeywords(){
+    const page = this
+    wx.getStorage({
+      key: 'searchKeywords',
+      success: function(res) {
+        page.setData({
+          keywords: res.data || []
+        })
+      },
+    })
+  },
+
+  addToLocalHistory: function(keyword) {
+    const page = this
+    var keywords = page.data.keywords
+    if (keywords.indexOf(keyword) < 0) {
+      keywords.unshift(keyword)
+      wx.setStorage({
+        key: 'searchKeywords',
+        data: keywords,
+        success() {
+          page.setData({
+            keywords: keywords
+          })
+        }
+      })
+    }
+  },
+
+  searchKeyword: function(event) {
+    this.go(event.currentTarget.dataset.keyword)
+  },
+
+  cleanKeywords: function() {
+    var page = this
+    wx.showModal({
+      content: '确认清空搜索历史？',
+      success(res) {
+        if (res.confirm) {
+          wx.removeStorage({
+            key: 'searchKeywords',
+            success: function(res) {
+              page.setData({
+                keywords: []
+              })
+            },
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
