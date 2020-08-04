8b4b1dd587b92cab82c2f76cc78a835cbdfc176f
enobj <laoji52125@163.com>
2020-08-04 16:36:38 +0800

    @图灵奖得主

--- cloudfunctions/dbUtils/index.js
+++ cloudfunctions/dbUtils/index.js
@@ -1,6 +1,6 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-const mpaUtils = require('./../../doc/wpa_content_utils.js')
+const fs = require("fs")
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
@@ -11,11 +11,32 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
 
-  const groups = event.groups || ['nbe-wlx','nbe-hx','nbe-slx-yx','nbe-wx','nbe-hp','nbe-jjx']
+  const groups = event.groups || ['turing']
 
   const res = await db.collection('mpa_content_group').where({
     _id: db.command.in(groups)
   }).get()
 
-  mpaUtils.writeToJsonFile(res.data, 'groups')
+  writeToJsonFile(res.data, 'patch/20200804/groups')
+}
+
+function writeToJsonFile(list, fileName) {
+  // 打开目标文件
+  fs.open(`/Users/jienhui/WeChatProjects/man-power-ai-wx/doc/init/${fileName}.json`, 'w', function (err, fd) {
+    // 一行行写入
+    var item = null
+    const listLength = list.length
+    while (item = list.shift()) {
+      if(list.length == 0 || list.length == listLength - 1){
+        console.log(`+++whiting ${list.length} : ${item.content}`)
+      }
+      fs.writeFileSync(fd, JSON.stringify(item) + '\n', {
+        flag: 'a'
+      })
+    }
+
+    fs.close(fd, function () {
+      console.log('写入文件完成')
+    })
+  })
 }
