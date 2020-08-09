064d57c670f8b683d5f0402792841e97bc2bb016
enobj <laoji52125@163.com>
2020-08-09 11:29:49 +0800

    @美女图集

---
+++ doc/crawler_meinv.js
@@ -0,0 +1,66 @@
+const mpaUtils = require('./wpa_content_utils.js')
+const cheerio = require('cheerio');
+
+loadItems()
+
+function loadItems(page = 1, list = []) {
+  mpaUtils.request(`http://www.win4000.com/mobile_2340_0_0_${page}.html`).then(html => {
+    const $ = cheerio.load(html)
+    // console.log(html)
+    $('body > div.main > div > div.w1180.clearfix > div.Left_bar > div.list_cont.Left_list_cont.Left_list_cont2 > div > div > div > ul > li').each((index, item) => {
+      list.push({
+        content: $(item).find('p').text().trim(),
+        group: 'meinv',
+        radioOptions: ['好看', '收藏了'],
+        image: $(item).find('img').attr('data-original'),
+        link: $(item).find('a').attr('href'),
+      })
+    })
+    console.log(list.length)
+    // 递归
+    if (page < 1) {
+      loadItems(++page, list)
+    } else {
+      // 加载套图
+      loadImages(list.slice(0)).then(res=>{
+        // 采集完成
+        console.log(`采集完成：${list.length}`)
+        console.log(list[0])
+        mpaUtils.writeToJsonFile(list, '../patch/20200809/meinv')
+      })
+    }
+  })
+}
+
+function loadImages(list) {
+  const item = list.pop()
+  return mpaUtils.request(item.link).then(html => {
+    // 清除原始地址
+    delete item.link
+    const $ = cheerio.load(html)
+    // 套图
+    const imgList = $('#scroll > li').map((index, ele)=>{
+      return $(ele).find('a').attr('href')
+    }).get()
+    return loadImage(imgList).then(images=>{
+      item.images = images
+      if (list.length) {
+        return loadImages(list)
+      }
+      return Promise.resolve()
+    })
+  })
+}
+
+function loadImage(list, result=[]){
+  const item = list.pop()
+  return mpaUtils.request(item).then(html => {
+    const $ = cheerio.load(html)
+    result.push($('#pic-meinv > a > img').attr('src'))
+
+    if (list.length) {
+      return loadImage(list, result)
+    }
+    return Promise.resolve(result)
+  })
+}
